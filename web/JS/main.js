94ef63ec545fa740d007dcf0a8d7a6699ac5be8d
anwang112 <anwang112@gmail.com>
2018-12-05 15:37:24 +0800

    RWD-menu-down

--- web/JS/main.js
+++ web/JS/main.js
@@ -1,9 +1,13 @@
-var member = document.querySelector('#memLogin');
+var member = document.querySelector('.memLogin');
+var member2 = document.querySelector('.memLogin2');
 var close = document.querySelector('#closeLogin');
 //登入視窗
 function showloginWin(){
     loginWin=document.querySelector('.loginWin');
     if(member.title=="登入"){
+        loginWin.style.display='block';
+    }
+    if(member2.title=="登入"){
         loginWin.style.display='block';
     }
 }
@@ -25,8 +29,30 @@ function openSubmenu(){
 }
 function init() {
     member.addEventListener('click',showloginWin,false);
+    member2.addEventListener('click',showloginWin,false);
     closeLogin.addEventListener('click',closeLoginF,false);
     mMenu.onclick=openSubmenu;
 }
 
 onload=init;
+
+//JQ
+$('#mobiMenu li').click(function(){
+    $(this).children('.submenu').stop(true)
+    
+    .slideToggle();
+    $('.submenu').not($(this).children('.submenu')).slideUp();
+});
+// $('#itemMenu').click(function(){
+//      $('#itemMenu .submenu').stop(true).slideToggle();
+
+// });
+// $('#mileMenu').click(function(){
+//     $('#mileMenu .submenu').stop(true).slideToggle();
+// });
+// $('#kcoinsMenu').click(function(){
+//     $('#kcoinsMenu .submenu').stop(true).slideToggle();
+// });
+// $('#actMenu').click(function(){
+//     $('#actMenu .submenu').stop(true).slideToggle();
+// });
